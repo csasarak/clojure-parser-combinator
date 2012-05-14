8743db5d450241c4c7e502aa338bf711c8dd551a
chris <chris.sasarak@gmail.com>
2012-05-14 01:33:56 -0400

    Completed the parser.clj library

--- software/parser.clj
+++ software/parser.clj
@@ -1,20 +1,54 @@
 ;; Author: Christopher Sasarak
 ;; This declares that these functions are in the parser namespace
 (ns parser "Basic functions for monadic parsing.") 
- 
+
 ;; For the purposes of these parsers, the results will be a vector of 2-vectors
 ;; One possible alternative is a Clojure facility such as defrecord which provides
 ;; some documentation of the expected values for a type and creates a Java class for
 ;; those items under the hood. e.g. (defrecord parseItem [consumed unconsumed])
 ;; See page 189 of "The Joy of Clojure" by Michael Fogus and Chris Houser
 
+(defn >>= [pars f]
+  "A bind function for parsers"
+  (fn [inp] (first (concat (for [[v inp'] (pars inp)]
+                             ((f v) inp'))))))
 
-;; FUNCTIONS FOR MANIPULATING PARSERS
 (defn success [v]
   "Success returns a parser which consumes none of its input and returns
    v as the result"
   (fn [inp] [[v inp]])) ;; fn creates a function that isn't bound to a name, defn
                         ;; is a combination of the def and fn macros for named functions
+
+(defn >> [p1 p2]
+  "Then is like bind but throws away the result of the first monad"
+  (>>= p1 (fn [inp]
+            p2)))
+
+(defn bind2 [p1 p2]
+  "Bind two parsers together in sequence"
+  (>>= p1 (fn [inp1]
+            (>>= p2 (fn [inp2]
+                      (success (concat [inp1 inp2])))))))
+
+(defn throwaway [p1]
+  "Run p1 and throw away whatever it returns"
+  (>> p1 (success [])))
+
+;; This function can be used to emulate Haskell's do notation for parsers.  It
+;; uses Clojure's loop-recur functionality which are macros that let you do tail
+;; recursion. The JVM does not allow for this to be done automatically.  The
+;; first loop call binds initial values to variables. The call to recur is
+;; supplied with new values for these variables and jumps up to the most recent
+;; recursion target it can find, in this case loop. The function itself can also
+;; be used as a target, i.e. use recur without loop.
+(defn bind-many [p1 p2 & parsers] 
+  "Bind a list of many parsers together in sequence"
+  (loop [[p3 & ps] parsers acc (bind2 p1 p2)]
+    (if (not p3) acc
+        (recur ps (bind2 acc p3))))
+  )
+
+;; FUNCTIONS FOR MANIPULATING PARSERS
 
 (defn zero [inp]
   "This parser always fails, regardless of its input"
@@ -41,10 +75,13 @@
   (p1 inp))
 
 (defn parse [p1 inp]
-  "Run a parser with the given input and return it as a 2-vector of Strings"
+  "Run a parser with the given input and return it as a 2-vector of Strings, or
+   nil if the text couldn't be parsed successfully"
   (let [[consumed left] (first (parse' p1 inp))]
-    [(apply str consumed) (apply str left)])
-  )
+    (if (seq consumed)
+      [(apply str (flatten consumed)) (apply str left)]
+      nil)
+    ))
 
 ;; This function has a slight advantage on the haskell implementation because it
 ;; can operate on an arbitrary number of parsers
@@ -69,6 +106,8 @@
     (first ((apply choice-succ parsers) inp))
     ))
 
+;; Because many1 and many are mutually recursive, using a declare directive tells
+;; clojure not to worry about it when it tries to interpret many and many1
 (declare many)
 (defn many1 [p]
   (>>= p (fn [inp1]
@@ -79,38 +118,6 @@
 
 (defn many [p]
   (x-or (many1 p) (success [])))
-
-
-;; FUNCTIONS FOR DEALING WITH MONADS
-(defn >>= [pars f]
-  "A bind function for parsers"
-  (fn [inp] (first (concat (for [[v inp'] (pars inp)]
-                             ((f v) inp'))))))
-
-(defn >> [p1 p2]
-  "Then is like bind but throws away the result of the first monad"
-  (>>= p1 (fn [inp]
-            p2)))
-
-(defn bind2 [p1 p2]
-  "Bind two parsers together in sequence"
-  (>>= p1 (fn [inp1]
-            (>>= p2 (fn [inp2]
-                      (success (flatten [inp1 inp2])))))))
-
-;; This function can be used to emulate Haskell's do notation for parsers.  It
-;; uses Clojure's loop-recur functionality which are macros that let you do tail
-;; recursion. The JVM does not allow for this to be done automatically.  The
-;; first loop call binds initial values to variables. The call to recur is
-;; supplied with new values for these variables and jumps up to the most recent
-;; recursion target it can find, in this case loop. The function itself can also
-;; be used as a target, i.e. use recur without loop.
-(defn bind-many [p1 p2 & parsers] 
-  "Bind a list of many parsers together in sequence"
-  (loop [[p3 & ps] parsers acc (bind2 p1 p2)]
-    (if (not p3) acc
-        (recur ps (bind2 acc p3))))
-  )
 
 ;; FUNCTIONS THAT MAKE PARSERS FOR DIFFERENT STRINGS
 
@@ -150,3 +157,55 @@
      "The parser which matches any upper or lower-case letter or a number"
      (x-or letter digit))
 
+(defn string [str]
+  "Return the parser that matches a specific string"
+  (fn [inp]
+    (let [parsers (map #(p-char %) (seq str))]
+      ((apply bind-many parsers) inp))))
+
+(def word
+     "A sequence of at least one letter"
+     (many1 letter))
+
+;; BASIC PARSER FOR S-EXPRESSIONS
+
+(defn isValidStringChar [c]
+  (not= c \"))
+;; A '\' followed by a character is the character represented by that character
+;; e.g. \c == 'c'
+
+(def string-p
+     "A string starts with a single \", followed by any number of characters
+     or whitespace and end with a \"."
+     (fn [inp]
+       ((bind-many (p-char \")
+                   (many (sat isValidStringChar))
+                   (p-char \")) inp))
+     )
+
+(declare sexp-p) 
+(def sexp-list-p (bind-many (many whitespaceChar)
+                            (x-or sexp-p word string-p)
+                         ))
+
+(defn outermost-sexps-p [inp]
+     "This is for the outermost s-expressions"
+     ((many (bind-many (many whitespaceChar)
+                       sexp-p
+                       (many whitespaceChar))) inp ))
+
+(defn sexp-p [inp]
+  "Parser for an non-toplevel s-expression"
+  ((bind-many (many whitespaceChar)
+              (x-or (p-char \( ) (string "'("))
+              (many whitespaceChar)
+              (many sexp-list-p)
+              (many whitespaceChar)
+              (p-char \) )) inp)
+  )
+
+
+(defn parse-s-expressions [str]
+  "This function will parse s-expressions, returning a vector of parsed text
+   and unparsed text."
+  (parse outermost-sexps-p str))
